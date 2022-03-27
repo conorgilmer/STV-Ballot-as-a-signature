df89a9ac9d4991bcc65debfed902f734fa268bc7
Conor Gilmer <conor.gilmer@gmail.com>
2022-03-27 11:13:33 +0100

    python merge 2 csv files

---
+++ python/concatcsv.py
@@ -0,0 +1,19 @@
+# importing pandas
+import pandas as pd
+
+#input file
+constituency="Test2002"
+#constituency="DublinNorth2002"
+#constituency="DublinWest2002"
+#constituency="Meath2002"
+reg_csv='../data/'+constituency+'.csv'
+irreg_csv='../data/'+constituency+'gen.csv'
+merged_csv='../data/'+constituency+'merged.csv'
+  
+# merging two csv files
+df = pd.concat(
+    map(pd.read_csv, [reg_csv, irreg_csv]), ignore_index=True)
+print(df)
+
+# write dataframe of 2 csv files to csv file
+df.to_csv(merged_csv)
