20629a4a6c5a257ae0833e621282fda3c62e15d9
Conor Gilmer <conor.gilmer@gmail.com>
2022-04-05 12:00:49 +0100

    processed 1/0 -> Reg Irreg

--- python/py/proceessseq.py
+++ python/py/proceessseq.py
@@ -1,19 +1,19 @@
 import pandas as pd
 
-
+#process for constituency change Seq to Sequence, and 1/0 to Regular /Irregular
 def process(constituency):
     in_csv  = "../../data/processed/"+constituency+"_merged.csv"
-    out_csv = "../../data/processed/"+constituency+"_out.csv"
+    out_csv = "../../data/processed/"+constituency+"_merged_out.csv"
     df = pd.read_csv(in_csv)
-    df['Sequence'] = df['Seq'].map({0: "Irregular", 1: "Reg"})
+    df['Sequence'] = df['Seq'].map({0: "Irregular", 1: "Regular"})
     df = df.drop(['Seq'], axis=1)
-    df.to_csv(out_csv)
 
-
+# main function
 def main():
     process("Meath2002")
     process("DublinNorth2002")
     process("DublinWest2002")
 
+# call main
 if __name__ == "__main__":
     main()
