45917cbcd3aad09a0138d44929bd5f5e3b2cba1c
Conor Gilmer <conor.gilmer@gmail.com>
2022-04-08 15:57:18 +0100

    info on sw & libaries

---
+++ python/py/vers.bat
@@ -0,0 +1,11 @@
+echo " VERSIONS "
+javac -version
+conda -V
+python --version
+perl -version | grep 'This is'
+cobc -version | grep 'cobc'
+fpc -v | grep 'Compiler'
+lua -v
+awk -version
+ant -version
+make -v | grep 'Make'
